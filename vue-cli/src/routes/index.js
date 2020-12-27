2673d6e6163e4c9592be9ef0a2a953da6aea245c
GalaDriel <g_ala@naver.com>
2020-12-27 23:18:26 +0900

    오타 <WordRelay -> WordGame> 수정

--- vue-cli/src/routes/index.js
+++ vue-cli/src/routes/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import WordRelay from '../../../02. 끝말잇기/components/WordGame'
+import WordGame from '../../../02. 끝말잇기/components/WordGame'
 import NumberBaseball from '../../../03. 숫자야구/components/NumberBaseball'
 import NumberPicker from '../../../04. 번호추첨/components/NumberPicker'
 import ResponsiveTest from '../../../05. 반응속도/components/ResponsiveTest'
